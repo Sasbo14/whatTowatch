75017eb00d693972b6b4bf6eaffe4064d6504567
larbi.chajai <larbi.chajai@uha.fr>
2024-02-15 17:37:47 +0100

    mise en place et connexion a la bdd + ajout entité user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {User} from "./entities/user.entity";
+import { FirebaseService } from './firebase/firebase.service';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'whattowatch',
+          entities: [User],
+          synchronize: true, // Utilisez `false` en production
+      })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirebaseService],
 })
 export class AppModule {}
